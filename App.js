970297060804206a2f341f21ef554a3d1c8d96af
BuildTools <unconfigured@null.spigotmc.org>
2023-02-17 14:34:47 -0500

    added home page and loign page

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react"
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './src/components/Home';
+import Login from './src/components/Login';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Router>
+
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/login'>Login</Link>
+        </nav>
+        
+        <StatusBar style="auto" />
+        
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/login" element={ <Login />} />
+
+        </Routes>
+
+
+      </Router>
     </View>
   );
 }
@@ -13,7 +31,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b2b2b2',
     alignItems: 'center',
     justifyContent: 'center',
   },
